b5e3dd96a57ac05904b657c8adfbb93f40fc7d8e
Raymaekers Luca <luca@spacehb.net>
2025-12-07 16:04:04 +0100

    Solution to part one.

--- 2025/day2/day2.c
+++ 2025/day2/day2.c
@@ -8,11 +8,89 @@
 // Mine
 #include "lr/lr.h"
 
+#define NUMBER_OF_CORES 6
+
+#if 0 // Disable asserts
+#undef Assert
+#define Assert(Expression)
+#endif
+
 //~ Layers
 // OS
 #include "os.h"
 #include "arenas.c"
 #include "lanes.c"
+
+s64 GetInvalIDsSumForRangePartOne(range_s64 IDRange)
+{
+    s64 InvalidIDsSum = 0;
+    
+    // NOTE(luca): We can cache a lot of these calculations
+    // - the digit count mostly stays the same
+    // - the factors for the digit count as well
+    for(s64 ID = IDRange.Min; ID <= IDRange.Max; ID += 1)
+    {
+        s64 DigitsCount = 0;
+        
+        // Count digits in ID
+        {                
+            s64 Value = ID;
+            while(Value)
+            {
+                Value /= 10;
+                DigitsCount += 1;
+            }
+        }
+        
+        for(u64 Divider = 1; Divider <= DigitsCount/2; Divider += 1)
+        {
+            if(DigitsCount%Divider == 0)
+            {
+                // This ID can be invalid
+                s64 GroupSize = Divider;
+                s64 GroupRepeatCount = DigitsCount/Divider;
+                
+                if(GroupRepeatCount%2 != 0)
+                {
+                    continue;
+                }
+                
+                s64 Shift = 10;
+                for(s64 Index = 1; Index < GroupSize; Index += 1) Shift *= 10;
+                
+                s64 GroupValue = ID%Shift;
+                
+                b32 IDIsValid = false;
+                s64 TempID = ID;
+                for(s64 Index = 0; Index < GroupRepeatCount; Index += 1)
+                {
+                    s64 Group = TempID%Shift;
+                    if(GroupValue != Group)
+                    {
+                        IDIsValid = true;
+                        break;
+                    }
+                    TempID /= Shift;
+                }
+                
+                if(!IDIsValid)
+                {
+                    
+#if 0                                
+                    OS_PrintFormat("[%ld] Invalid ID: %ld\n", LaneIndex(), ID);
+#endif
+                    
+                    InvalidIDsSum += ID;
+                    break;
+                }
+                
+            }
+        }
+        
+    }
+    
+    return InvalidIDsSum;
+}
 
 ENTRY_POINT(EntryPoint)
 {
@@ -27,6 +105,105 @@ ENTRY_POINT(EntryPoint)
         ThreadName.Size = 1;
         ThreadName.Data[0] = (u8)LaneIndex() + '0';
         OS_SetThreadName(ThreadName);
+    }
+    
+    s64 InvalidIDsSum = 0;
+    
+    str8 InputFile = OS_ReadEntireFileIntoMemory("./2025/day2/input");
+    
+    range_s64 Range = LaneRange(InputFile.Size);
+    
+    if(LaneIndex() == 0)
+    {
+        OS_PrintFormat("Input size: %lu\n", InputFile.Size);
+    }
+    
+    u8 *In = InputFile.Data;
+    
+    // 1. Parse range
+    // 2. Check for invalid IDs
+    // 3. Sum all invalid IDs
+    // 4. Aggregate
+    
+    s64 At = Range.Min;
+    
+    // Walk back to previous comma if range does not start on one
+    if(At != 0)
+    {
+        while(At >= 0 && In[At] != ',') At -= 1;
+        if(At < 0)
+        {
+            OS_PrintFormat("ERROR(%d): Expected ',' found BOF\n", Range.Min);
+            return 0;
+        }
+        At += 1;
+    }
+    
+    for(; At < Range.Max; At += 1)
+    {
+        range_s64 IDRange = {0};
+        
+        s64 ParsedValue = 0;
+        
+        while(In[At] != '-')
+        {
+            Assert(In[At] >= '0' && In[At] <= '9');
+            s64 Digit = (In[At] - '0');
+            ParsedValue = 10*ParsedValue + Digit; 
+            At += 1;
+        }
+        IDRange.Min = ParsedValue;
+        
+        At += 1;
+        
+        ParsedValue = 0;
+        while(At < Range.Max && In[At] != ',')
+        {
+            Assert(In[At] >= '0' && In[At] <= '9');
+            s64 Digit = (In[At] - '0');
+            ParsedValue = 10*ParsedValue + Digit; 
+            
+            At += 1;
+        }
+        IDRange.Max = ParsedValue;
+        
+        if(In[At] == ',' || At == InputFile.Size)
+        {
+#if 0
+            // To validate
+            // TODO(luca): thread-safe OS_PrintFormat ?
+            printf("%ld-%ld\n", IDRange.Min, IDRange.Max);
+#endif
+            
+            InvalidIDsSum += GetInvalIDsSumForRangePartOne(IDRange);
+        }
+    }
+    
+#if 0    
+    OS_PrintFormat("%ld: Sum: %ld\n", LaneIndex(), InvalidIDsSum);
+#endif
+    
+    s64 *Sums = 0;
+    if(LaneIndex() == 0)
+    {
+        Sums = PushArray(ThreadContext->Arena, s64, LaneCount()); 
+    }
+    LaneSyncU64((u64 *)&Sums, 0);
+    
+    Sums[LaneIndex()] = InvalidIDsSum;
+    
+    LaneSync();
+    s64 TotalSum = 0;
+    for(EachIndex(Index, LaneCount()))
+    {
+        TotalSum += Sums[Index];
+    }
+    
+    // 1227775554
+    
+    if(LaneIndex() == 0)
+    {
+        OS_PrintFormat("Invalid IDs sum: %ld\n", TotalSum);
     }
     
     return 0;
