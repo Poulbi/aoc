744269b74a87614449a0a007fef62cd3cc17d89f
Raymaekers Luca <luca@spacehb.net>
2025-12-10 11:56:15 +0100

    Added day3 in CUDA

---
+++ 2025/day3/day3.cu
@@ -0,0 +1,112 @@
+#include <cuda_runtime.h>
+
+#include "base/base.h"
+
+#define BatteryCount 12
+
+#define GPU_Assert(Expression) if(!(Expression)) { *(int *)0 = 0; }
+
+#if 1
+# define CU_Check(Expression) { CU_Check_((Expression), __FILE__, __LINE__); }
+#else
+# define CU_Check(Expression) Expression
+#endif
+inline void CU_Check_(cudaError_t Code, char *FileName, s32 Line, b32 Abort=false)
+{
+    if(Code != cudaSuccess) 
+    {
+        fprintf(stderr,"ERROR: %s %s %d\n", cudaGetErrorString(Code), FileName, Line);
+        if(Abort) 
+        {
+            exit(Code);
+        }
+    }
+}
+
+__global__ void
+GetBigJoltage(u64 *Output, u8 *Lines, s32 LinesCount, s32 LineSize)
+{
+    u8 Idx = blockIdx.x*blockDim.x + threadIdx.x;
+    if(Idx < LinesCount)
+    {
+        u8 *In = Lines + LineSize*Idx;
+        u64 *Out = Output + Idx;
+        
+        u64 Number = 0;
+        s32 LastFoundPos = 0;
+        for(s32 BatteryIdx = 0; BatteryIdx < BatteryCount; BatteryIdx += 1)
+        {
+            s32 Start = LastFoundPos;
+            s32 End = LineSize - (BatteryCount - BatteryIdx);
+            
+            // NOTE(luca): There are no zeroes in the input.
+            u8 Digit = '0';
+            for(s32 At = Start; At < End; At += 1)
+            {
+                GPU_Assert(In[At] >= '0' && In[At] <= '9');
+                if(In[At] > Digit)
+                {
+                    Digit = In[At];
+                    LastFoundPos = At + 1;
+                }
+            }
+            
+            Number = Number*10 + (u64)(Digit - '0');
+        }
+        
+        *Out = Number;
+    }
+}
+
+ENTRY_POINT(EntryPoint)
+{
+    if(LaneIndex() == 0)
+    {
+        // We know how long each line will be.
+        str8 File = OS_ReadEntireFileIntoMemory("./data/day3_input");
+        
+        umm LineSize = 0;
+        for(umm Idx = 0; Idx < File.Size; Idx += 1)
+        {
+            if(File.Data[Idx] == '\n')
+            {
+                LineSize = Idx + 1;
+                break;
+            }
+        }
+        Assert(LineSize < File.Size);
+        Assert(File.Size%LineSize == 0);
+        
+        s32 LinesCount = (s32)(File.Size / LineSize); 
+        s32 ThreadsPerBlock = 32;
+        s32 BlocksCount = (LinesCount/ThreadsPerBlock) + 1;
+        
+        umm OutputSize = (sizeof(u64)*BatteryCount*LinesCount);
+        u64 *HostOutput = (u64 *)ArenaPush(ThreadContext->Arena, OutputSize);
+        u64 *DeviceOutput = 0;
+        u8 *DeviceLines = 0;
+        
+        CU_Check(cudaMalloc(&DeviceLines, File.Size));
+        CU_Check(cudaMalloc(&DeviceOutput, OutputSize));
+        CU_Check(cudaMemcpy(DeviceLines, File.Data, File.Size, cudaMemcpyHostToDevice));
+        
+        GetBigJoltage<<<BlocksCount, ThreadsPerBlock>>>(DeviceOutput, DeviceLines, LinesCount, (s32)LineSize);
+        CU_Check(cudaGetLastError()); 
+        
+        CU_Check(cudaDeviceSynchronize());
+        CU_Check(cudaMemcpy(HostOutput, DeviceOutput, OutputSize, cudaMemcpyDeviceToHost));
+        
+        u64 JoltageSum = 0;
+        for(s32 Idx = 0; Idx < LinesCount; Idx += 1)
+        {
+            JoltageSum += HostOutput[Idx];
+#if 0
+            OS_PrintFormat("%lu\n", HostOutput[Idx]);
+#endif
+        }
+        
+        OS_PrintFormat("Joltage sum is %lu.\n", JoltageSum);
+    }
+    
+    return 0;
+}
